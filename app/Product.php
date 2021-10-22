9cad17b773ee9a65e5090f828b262d9797417d69
ThomasvanGastel2 <71122683+ThomasvanGastel2@users.noreply.github.com>
2021-10-22 10:09:32 +0200

    .

---
+++ app/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public function types()
+    {
+    	return $this->hasMany('App\Type');
+    }
+    public function getPriceAttribute($value)
+    {
+        $discount = $value * ($this->discount/100);//Kortingineuro's
+        $final_price=$value-$discount;//Haalkortingafvanprijs
+        return number_format($final_price,2);//Zorgaltijdvoor2decimalen
+    }
+}
